d9cd52f36df7569088e19ce80386e16d070ec4d5
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-06-09 15:38:37 +0100

    serverless setup complete

--- tasks.js
+++ tasks.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/tasks", function(req, res) {
   res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
+
+app.post("/tasks", function(req, res) {
+  const text = req.body.text;
+  const date = req.body.date;
+
+  res.json({
+    message: `Received a request to add task ${text} with date ${date}`
+  });
+});
+
+app.delete("/tasks/:taskId", function(req, res) {
+  const id = req.params.taskId;
+
+  // Respond to the request somehow
 });
 
 module.exports.handler = serverless(app);
