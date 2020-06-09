7d470c84852dfc609d0d2e2e69c74b57795fafdc
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-06-09 15:15:09 +0100

    updated function/infrastructure

---
+++ tasks.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/tasks", function(req, res) {
+  res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
+
+module.exports.handler = serverless(app);
